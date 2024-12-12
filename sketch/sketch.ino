cbc687ef95f72dc30c5f7a12c22b9038814429e1
zb656 <145730190+zb656@users.noreply.github.com>
2024-12-11 23:59:04 -0500

    trying to send data

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,14 +1,46 @@
+// Using: ArduinoJson by Benoit Blanchon
+// v6.21.5
+#include <ArduinoJson.h>
+
 int pbutton0;
 int pbutton1;
 int pbutton2;
 int pbutton3;
 int pbutton4;
 
+int LED0;
+
+void receiveData(const char* jsonText) {
+    StaticJsonDocument<128> resJson;
+    DeserializationError error = deserializeJson(resJson, jsonText);
+
+    if (error) {
+        Serial.println(F("deserializeJson() failed: "));
+        Serial.println(error.f_str());
+        return;
+    }
+
+    int s0 = resJson["data"]["s0"];
+    int s1 = resJson["data"]["s1"];
+    int s2 = resJson["data"]["s2"];
+    int s3 = resJson["data"]["s3"];
+    int s4 = resJson["data"]["s4"];
+
+    int mSequence[5] = {s0, s1, s2, s3, s4};
+
+    for(int i = 0; i < 4; i++) {
+        Serial.println(mSequence[i]);
+    }
+}
+
 void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT);
-  pinMode(5, INPUT);
-  //other button pinModes
+  pinMode(12, INPUT);
+  pinMode(11, INPUT);
+  pinMode(10, INPUT);
+  pinMode(9, INPUT);
+  pinMode(8, INPUT);
+  pinMode(5, OUTPUT);
 
   pbutton0 = 0;
   pbutton1 = 0;
@@ -21,11 +53,16 @@ void setup() {
 //AND THEN MAKE A FOR LOOP WHERE FOR THAT ARRAY IT LIGHTS UP THE RELEVANT LEDS
 
 void loop() {
-  int button0 = digitalRead(2);
-  int button1 = digitalRead(5);
-  int button2 = //digitalRead(5);
-  int button3 = //digitalRead(5);
-  int button4 = //digitalRead(5);
+  int button0 = digitalRead(12);
+  int button1 = digitalRead(11);
+  int button2 = digitalRead(10);
+  int button3 = digitalRead(9);
+  int button4 = digitalRead(8);
+
+  if (Serial.available() > 0) {
+    String jsonText = Serial.readStringUntil('\n');
+    receiveData(jsonText.c_str());
+  }
 
 //ADD INTO THESE BUTTONS CODE TO LIGHT UP THE RELEVANT LEDS
   if (button0 == 1 && pbutton0 == 0) {
--- sketch/sketch.js
+++ sketch/sketch.js
@@ -13,7 +13,9 @@ let buttonVal;
 let mSequence = [];
 let mAnswer = [];
 
-function preload() {
+let points;
+
+function preload() { //SOUNDS
   sound0 = loadSound("../assets/sound0.wav");
   sound1 = loadSound("../assets/sound1.wav");
   sound2 = loadSound("../assets/sound2.wav");
@@ -21,24 +23,41 @@ function preload() {
   sound4 = loadSound("../assets/sound4.wav");
 }
 
+function sendSerial(data) {
+  if (mSerial.opened()) {
+    mSerial.write(JSON.stringify(data));
+    mSerial.write("\n");
+  }
+}
+
 function connectToSerial() {
   if (!mSerial.opened()) {
     mSerial.open(9600);
     connectButton.hide();
+
+    startGame = createButton("Start the Game");
+    startGame.position(width/2, height/4);
+    startGame.mousePressed(createGameArray);
   }
 }
 
 //TO DO: NEED CODE TO SEND THIS ARRAY TO ARDUINO
-function createGameArray() {
-  //GENERATES RANDOM NUMBERS FOR ARRAY
+//TO DO: FIX THE FACT THAT ALL THE SOUNDS PLAY AT ONCE
+function createGameArray() { //generates random sequence for sounds
+  startGame.hide();
+
+  resetGame = createButton("Restart");
+  resetGame.position(200, 100);
+  resetGame.mousePressed(setup);
+  
   for (let i = 0; i < 5; i++) {
     let randomNumber = random(['0', '1', '2', '3', '4']);
     mSequence.push(randomNumber);
   }
 
   print(mSequence);
+  sendSerial({data: { s0: mSequence[0], s1: mSequence[1], s2: mSequence[2], s3: mSequence[3], s4: mSequence[4]}});
 
-  //PLAY SOUNDS
   for (let idx = 0; idx < mSequence.length; idx++) {
     if (mSequence[idx] == 0) {
       sound0.play();
@@ -59,7 +78,21 @@ function createGameArray() {
     if (mSequence[idx] == 4) {
       sound4.play();
     }
-    //SHOULD THIS BE IN HERE OR IN DRAW?
+  }
+}
+
+function checkAnswer() {
+  let mSequenceString = mSequence.toString();
+  let mAnswerString = mAnswer.toString();
+
+  if (mSequenceString == mAnswerString) {
+    points++;
+    mSequence.length = 0;
+    mAnswer.length = 0;
+  } else {
+    points = 0;
+    mSequence.length = 0;
+    mAnswer.length = 0;
   }
 }
 
@@ -69,46 +102,29 @@ function setup() {
   mSerial = createSerial();
   connectButton = createButton("Connect To Serial");
   connectButton.position(width / 2, height / 2);
-  connectButton.mousePressed(connectToSerial);
+  connectButton.mousePressed(connectToSerial);  
 
-  //GENERATE RANDOM SEQUENCE
-  createGameArray();
+  points = 0;
 }
-
-//so while the sounds are playing, the lights next to the respective buttons turn on
-//i think a for loop? so you have the randomly generated numbers in an index, then the for loop
-//plays each note for each number and sends signal to arduino to light up the LEDs
-//or just have something in the code permanently that says when this sound plays then this light lights up
-
-//and then in arduino when a button is pressed it plays the sound and adds the number to a second index
-//and that index is checked to see if its the same index as the first index. if it is a point is added 
-//and then no matter if its correct or not
-//the indexes are cleared and the first index has new random numbers in it.
-//i believe you clear an array by saying [arrayname].length = 0.
-
-
-// function createUserArray() {
-//   //similar to above array but its filled in by the numbers returned by arduino
-//   //or should this go in the if buttonval = part where if that button is pushed
-//   //that number is added to the user array
-//   //and if the user array reaches over a length of 5
-//   //then the answer is checked and the length is reset to 0. yeah?
-// }
 
 function draw() {
   background(0);
+  fill(255);
+  textSize(50);
+  text("Points:" + " " + points, width/3, height/2);
   
   if (mSerial.opened() && mSerial.availableBytes() > 0) {
-  let mLine = mSerial.readUntil("\n"); //i think i can delete the let here
-  // print(mLine);
+  mLine = mSerial.readUntil("\n");
 
-  let buttonVal = int(mLine); //i think i can delete the let here
+  buttonVal = int(mLine);
   print(buttonVal);
 
   //CAN ALL OF THIS BE MADE INTO A CLASS? LEAVE THAT TIL LAST TO SEE IF IT WORKS
+  //okay if you take this out of the if statement it just repeats forever and forever.
+  //but you might be able to make it a function?
   if (buttonVal == 0) {
     sound0.play();
-    mAnswer.push('0'); //should it be quotes or not?
+    mAnswer.push('0');
   }
 
   if (buttonVal == 1) {
@@ -131,17 +147,14 @@ function draw() {
     mAnswer.push('4');
   }
 
-
+  print(mAnswer);
+  }
 
   //code to say if [sound] is playing send signal to arduino to turn on [LED]
   //would this go somewhere else because its sending signal rather than receiving?
+
+  if (mAnswer.length == 5) {
+    checkAnswer();
+    createGameArray();
   }
 }
-
-//but im unsure how to go about having the computer check that the notes played are correct
-
-
-//also my week will be very busy with writing 15pg of my thesis...
-//so maybe if that grows to be too much this project gets scaled down into being a piano
-//with some visuals on computer screen situation?
-//or just doesn't have the game aspect of checking correct/incorrect
