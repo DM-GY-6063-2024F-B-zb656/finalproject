c0253b76a1d09dda5597c59bcf4e445210467611
zb656 <145730190+zb656@users.noreply.github.com>
2024-12-11 21:33:34 -0500

    working w arrays

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,18 +1,36 @@
+int pbutton0;
 int pbutton1;
 int pbutton2;
+int pbutton3;
+int pbutton4;
 
 void setup() {
   Serial.begin(9600);
   pinMode(2, INPUT);
   pinMode(5, INPUT);
+  //other button pinModes
 
+  pbutton0 = 0;
   pbutton1 = 0;
   pbutton2 = 0;
+  pbutton3 = 0;
+  pbutton4 = 0;
 }
 
+//NEED CODE TO RECEIVE THE mSequence ARRAY FROM P5
+//AND THEN MAKE A FOR LOOP WHERE FOR THAT ARRAY IT LIGHTS UP THE RELEVANT LEDS
+
 void loop() {
-  int button1 = digitalRead(2);
-  int button2 = digitalRead(5);
+  int button0 = digitalRead(2);
+  int button1 = digitalRead(5);
+  int button2 = //digitalRead(5);
+  int button3 = //digitalRead(5);
+  int button4 = //digitalRead(5);
+
+//ADD INTO THESE BUTTONS CODE TO LIGHT UP THE RELEVANT LEDS
+  if (button0 == 1 && pbutton0 == 0) {
+    Serial.println("0");
+  }
 
   if (button1 == 1 && pbutton1 == 0) {
     Serial.println("1");
@@ -22,8 +40,19 @@ void loop() {
     Serial.println("2");
   }
 
+  if (button3 == 1 && pbutton3 == 0) {
+    Serial.println("3");
+  }
+
+  if (button4 == 1 && pbutton4 == 0) {
+    Serial.println("4");
+  }
+
+  pbutton0 = button0;
   pbutton1 = button1;
   pbutton2 = button2;
+  pbutton3 = button3;
+  pbutton4 = button4;
 
   delay(2);
 }
--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,21 +1,24 @@
 let mSerial;
 let connectButton;
 
+let sound0;
 let sound1;
 let sound2;
 let sound3;
 let sound4;
-let sound5;
 
 let mLine;
 let buttonVal;
 
+let mSequence = [];
+let mAnswer = [];
+
 function preload() {
+  sound0 = loadSound("../assets/sound0.wav");
   sound1 = loadSound("../assets/sound1.wav");
   sound2 = loadSound("../assets/sound2.wav");
   sound3 = loadSound("../assets/sound3.wav");
   sound4 = loadSound("../assets/sound4.wav");
-  sound5 = loadSound("../assets/sound5.wav");
 }
 
 function connectToSerial() {
@@ -25,55 +28,113 @@ function connectToSerial() {
   }
 }
 
+//TO DO: NEED CODE TO SEND THIS ARRAY TO ARDUINO
+function createGameArray() {
+  //GENERATES RANDOM NUMBERS FOR ARRAY
+  for (let i = 0; i < 5; i++) {
+    let randomNumber = random(['0', '1', '2', '3', '4']);
+    mSequence.push(randomNumber);
+  }
+
+  print(mSequence);
+
+  //PLAY SOUNDS
+  for (let idx = 0; idx < mSequence.length; idx++) {
+    if (mSequence[idx] == 0) {
+      sound0.play();
+    }
+
+    if (mSequence[idx] == 1) {
+      sound1.play();
+    }
+
+    if (mSequence[idx] == 2) {
+      sound2.play();
+    }
+
+    if (mSequence[idx] == 3) {
+      sound3.play();
+    }
+
+    if (mSequence[idx] == 4) {
+      sound4.play();
+    }
+    //SHOULD THIS BE IN HERE OR IN DRAW?
+  }
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   mSerial = createSerial();
-
   connectButton = createButton("Connect To Serial");
   connectButton.position(width / 2, height / 2);
   connectButton.mousePressed(connectToSerial);
+
+  //GENERATE RANDOM SEQUENCE
+  createGameArray();
 }
 
-//for game need it to generate a string of numbers from 1-5
-//convert it to an index
-//so it generates say 2 3 1 4 5
-//and plays sounds 2, 3, 1, 4, 5 in that order
-//and sends that series of numbers to arduino
 //so while the sounds are playing, the lights next to the respective buttons turn on
+//i think a for loop? so you have the randomly generated numbers in an index, then the for loop
+//plays each note for each number and sends signal to arduino to light up the LEDs
+//or just have something in the code permanently that says when this sound plays then this light lights up
 
-//then the user repeats the sequence by pressing the buttons and plays the notes again
-//which is the code below
+//and then in arduino when a button is pressed it plays the sound and adds the number to a second index
+//and that index is checked to see if its the same index as the first index. if it is a point is added 
+//and then no matter if its correct or not
+//the indexes are cleared and the first index has new random numbers in it.
+//i believe you clear an array by saying [arrayname].length = 0.
+
+
+// function createUserArray() {
+//   //similar to above array but its filled in by the numbers returned by arduino
+//   //or should this go in the if buttonval = part where if that button is pushed
+//   //that number is added to the user array
+//   //and if the user array reaches over a length of 5
+//   //then the answer is checked and the length is reset to 0. yeah?
+// }
 
 function draw() {
   background(0);
   
   if (mSerial.opened() && mSerial.availableBytes() > 0) {
-  let mLine = mSerial.readUntil("\n");
+  let mLine = mSerial.readUntil("\n"); //i think i can delete the let here
   // print(mLine);
 
-  let buttonVal = int(mLine);
+  let buttonVal = int(mLine); //i think i can delete the let here
   print(buttonVal);
+
+  //CAN ALL OF THIS BE MADE INTO A CLASS? LEAVE THAT TIL LAST TO SEE IF IT WORKS
+  if (buttonVal == 0) {
+    sound0.play();
+    mAnswer.push('0'); //should it be quotes or not?
+  }
 
   if (buttonVal == 1) {
     sound1.play();
+    mAnswer.push('1');
   }
 
   if (buttonVal == 2) {
     sound2.play();
+    mAnswer.push('2');
   }
 
   if (buttonVal == 3) {
     sound3.play();
+    mAnswer.push('3');
   }
 
   if (buttonVal == 4) {
     sound4.play();
+    mAnswer.push('4');
   }
 
-  if (buttonVal == 5) {
-    sound5.play();
-  }
+
+
+  //code to say if [sound] is playing send signal to arduino to turn on [LED]
+  //would this go somewhere else because its sending signal rather than receiving?
   }
 }
 
